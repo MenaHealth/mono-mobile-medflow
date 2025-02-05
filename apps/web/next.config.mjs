94c15ef13e50e53fe56945588557e7e3332bea78
Kyle Essenmacher <kessenmacher7832@gmail.com>
2025-02-05 00:00:23 -0500

    passing env vars at build time correctly

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     transpilePackages: ["@repo/ui"],
+    env: {
+        MONGODB_URI: process.env.MONGODB_URI,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    },
     images: {
         unoptimized: true,  // Bypass Next.js image optimization
         domains: [
